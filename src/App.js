a188be1981e70521a08fea3077071ce5ce75a8f6
Bit D13 <bit@d13.co>
2023-06-30 07:26:45 +0200

    text

--- src/App.js
+++ src/App.js
@@ -35,6 +35,18 @@ function Page() {
   const { activeAccount, providers, disconnect } = useWallet();
 
   return <div>
+    <h2>Folks Finance Consensus Participation</h2>
+    <hr/>
+    <div className="small">
+      <p>
+        This microsite allows you to issue "keyreg" transactions through your Folks Finance escrow account, which enables your Algorand node to participate in consensus using your Folks Finance commitment.
+      </p>
+      <p>
+        See the <a href="https://docs.folks.finance/developer/official-sdks/participating-in-consensus">Folks Finance documentation on Consensus participation</a> for details.
+      </p>
+      <p>By <a href="https://twitter.com/d13_co">@D13_co</a></p>
+    </div>
+    <hr/>
     <Account />
     { activeAccount ? <Folks /> : null }
   </div>
@@ -94,6 +106,8 @@ function Folks() {
           algosdk.encodeUnsignedTransaction(txn)
         ]);
         await sendTransactions(signed);
+        alert('Success!');
+        setTimeout(() => setCounter(c => c + 1), 1000);
       } catch(e) {
         alert(e.message);
       }
@@ -126,7 +140,7 @@ function Folks() {
       <p>Your account is online!</p>
       <button onClick={goOffline}>Go Offline</button>
     </div> :
-    <>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
       <table>
 
         <tr><td>
@@ -170,7 +184,7 @@ function Folks() {
         </td></tr>
 
       </table>
-    </>}
+    </div>}
   </div> : null;
 }
 
