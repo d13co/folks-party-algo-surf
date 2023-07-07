26293ed7221849d92bf512cf1feba42c1070a01a
Bit D13 <bit@d13.co>
2023-07-07 13:37:27 +0300

    note about not needed

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -15,6 +15,13 @@ export default function Page() {
       <p>
         See the <a href="https://docs.folks.finance/developer/official-sdks/participating-in-consensus">Folks Finance documentation on Consensus participation</a> for details.
       </p>
+      <hr />
+      <p><strong>
+        The Folks Finance site now supports these transactions, so you should not need to use this site.</strong>
+      </p>
+      <p>
+        Check the <a href="https://app.folks.finance/algo-liquid-governance">Algo Liquid Governance</a> page ➡ Your Commitment ➡ Consensus (top right)
+      </p>
       <p>By <a href="https://twitter.com/d13_co">@D13_co</a></p>
     </div>
     <hr/>
